ae796d94699d8845b09ac7955a3f1b39697d538f
thiennv <risnguyen1993@gmail.com>
2022-09-08 17:09:55 +0700

    first commit

---
+++ AdminProduct/src/main/java/com/AdminProduct/AdminProduct/AdminProductApplication.java
@@ -0,0 +1,13 @@
+package com.AdminProduct.AdminProduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminProductApplication.class, args);
+	}
+
+}
